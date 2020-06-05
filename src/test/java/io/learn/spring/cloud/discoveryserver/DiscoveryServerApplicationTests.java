a12d818f93856f618598fefd33565d0bceecf763
Harika Pulavarthi <pulavarthiharika@gmail.com>
2020-06-05 12:24:11 +0530

    Initial commit

---
+++ src/test/java/io/learn/spring/cloud/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.learn.spring.cloud.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
